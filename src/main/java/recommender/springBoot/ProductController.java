ec6142eca42d476b8344d91617e92cad726ceece
ilia-java <iliabagheri1382@gmail.com>
2023-02-26 13:05:51 +0330

    Initial commit

---
+++ src/main/java/recommender/springBoot/ProductController.java
@@ -0,0 +1,22 @@
+package recommender.springBoot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping(value = "/product")
+    public List<Product> getProduct() {
+        List<Product> products = productService.findAll();
+        return products;
+    }
+
+}
+
+
